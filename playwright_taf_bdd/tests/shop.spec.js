15f607a06e1f85761629eac79dd0ba4f21a02e03
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-08-15 21:51:38 +0500

    Module-5: initial setup

---
+++ playwright_taf_bdd/tests/shop.spec.js
@@ -0,0 +1,95 @@
+import { test, expect  } from '../fixtures/pageFixtures';
+import { getPriceLabelForPrices, handleCheckoutAlert } from '../utils/helpers';
+import testData from './testData';
+
+test.beforeEach(async ({ shopPage }) => {
+  await shopPage.open();
+  await expect(shopPage.productCards).not.toHaveCount(0);
+});
+
+test('Verify default product cards count', async ({ shopPage  }) => {
+  await expect(shopPage.page).toHaveTitle(testData.shopPageTitle);
+  await expect(shopPage.productCards).toHaveCount(testData.defaultProductsCount);
+});
+
+test('Product can be added to cart', async ({ shopPage  }) => {
+  await test.step('When product is  added to cart', async () => {
+    await shopPage.addProductToCart(testData.products[0].name);
+  });
+
+  await test.step('Then the cart should show added product', async () => {
+    await expect(shopPage.cart.contentBlock).toBeVisible();
+    const productRow = shopPage.getProductRowInCart(testData.products[0].name);
+    await expect(productRow.rootEl).toBeVisible();
+  });
+
+  await test.step('And the cart should show products counter', async () => {
+    await expect(shopPage.cart.counter).toHaveText('1');
+  });
+
+  await test.step('When cart is closed', async () => {
+    await shopPage.cart.closeButton.click();
+    await expect(shopPage.cart.contentBlock).toBeHidden();
+  });
+
+  await test.step('Then the cart should show products counter', async () => {
+    await expect(shopPage.cart.counter).toHaveText('1');
+  });
+});
+
+test('Subtotal calculated correctly when products added to cart', async ({ shopPage  }) => {
+  let firstProductRow, secondProductRow;
+
+  await test.step('When first product is added to cart', async () => {
+    await shopPage.addProductToCart(testData.products[0].name);
+    await expect(shopPage.cart.contentBlock).toBeVisible();
+  });
+
+  await test.step('And second product is added to cart', async () => {
+    await shopPage.addProductToCart(testData.products[1].name);
+  });
+
+  await test.step('Then both products should be visible in the cart', async () => {
+    firstProductRow = shopPage.getProductRowInCart(testData.products[0].name);
+    secondProductRow = shopPage.getProductRowInCart(testData.products[1].name);
+    await expect(firstProductRow.rootEl).toBeVisible();
+    await expect(secondProductRow.rootEl).toBeVisible();
+  });
+
+  await test.step('And product prices should be displayed correctly', async () => {
+    await expect(firstProductRow.priceLabel).toHaveText(getPriceLabelForPrices(testData.products[0].price));
+    await expect(secondProductRow.priceLabel).toHaveText(getPriceLabelForPrices(testData.products[1].price));
+    await expect(shopPage.cart.productRows).toHaveCount(2);
+  });
+
+  await test.step('And the cart should show correct products count', async () => {
+    await expect(shopPage.cart.counter).toHaveText('2');
+  });
+
+  await test.step('Then the subtotal should be calculated correctly', async () => {
+    await expect(shopPage.cart.subTotalLabel).toHaveText(getPriceLabelForPrices([testData.products[0].price, testData.products[1].price]));
+  });
+});
+
+test('Verify checkout alert with one product', async ({ shopPage  }) => {
+  const checkoutAlert = handleCheckoutAlert(shopPage.page, testData.products[0].price);
+
+  await test.step('When product is added to cart', async () => {
+    await shopPage.addProductToCart(testData.products[0].name);
+    await expect(shopPage.cart.contentBlock).toBeVisible();
+  });
+
+  await test.step('Then the product should be visible in the cart', async () => {
+    const productRow = shopPage.getProductRowInCart(testData.products[0].name);
+    await expect(productRow.rootEl).toBeVisible();
+  });
+
+  await test.step('When checkout button is clicked', async () => {
+    await shopPage.cart.checkoutButton.click();
+  });
+
+  await test.step('Then checkout alert is shown with correct message', async () => {
+    await checkoutAlert;
+  });
+});
+
---
+++ playwright_taf_bdd/utils/helpers.js
@@ -0,0 +1,28 @@
+import { expect } from '@playwright/test';
+
+export const getPriceLabelForPrices = (prices) => {
+  let priceLabels = Array.isArray(prices) ? prices : [prices];
+  const sum = priceLabels
+    .reduce((acc, priceLabel) => acc + parseFloat(priceLabel), 0)
+    .toFixed(2);
+  return new RegExp(`^\\$\\s*${sum}`);
+}
+
+export async function handleCheckoutAlert(page, subtotal) {
+  return new Promise((resolve, reject) => {
+    let timeoutId;
+    timeoutId = setTimeout(() => {
+      reject(new Error('Dialog was not triggered within the timeout period.'));
+    }, 5000);
+    page.once('dialog', async (dialog) => {
+      clearTimeout(timeoutId);
+      try {
+        expect(dialog.message()).toBe(`Checkout - Subtotal: $ ${subtotal}`);
+        await dialog.accept();
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+}
