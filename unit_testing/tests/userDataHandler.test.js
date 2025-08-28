0465e137b5f7b1e61aeb5a5720420095f5c516e2
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-08-28 23:17:12 +0500

    Module 6: refactored tests

--- unit_testing/tests/userDataHandler.test.js
+++ unit_testing/tests/userDataHandler.test.js
@@ -4,7 +4,6 @@ const userData = require('../data/users.json')
 
 const testUserData = userData[1]
 let userDataHandler
-let loadedUsers
 
 function setupMock () {
   if (process.env.MOCK) {
@@ -13,34 +12,50 @@ function setupMock () {
       .reply(200, userData)
   }
 }
-// Helper function to load users once and reuse the data
-function setupLoadedUsers () {
-  beforeAll(async () => {
-    const tempHandler = new UserDataHandler()
+
+describe('when users were not loaded', () => {
+  beforeEach(async () => {
     setupMock()
-    await tempHandler.loadUsers()
-    loadedUsers = tempHandler.users
+    userDataHandler = new UserDataHandler()
+  })
+
+  afterEach(() => {
     nock.cleanAll()
   })
-  beforeEach(() => {
-    userDataHandler.users = [...loadedUsers]
-  })
-}
 
-beforeEach(() => {
-  userDataHandler = new UserDataHandler()
-  setupMock()
-})
-
-afterEach(() => {
-  nock.cleanAll()
-})
-
-describe('General tests', () => {
   test('loadUsers should load users', async () => {
     await userDataHandler.loadUsers()
     expect(userDataHandler.users.length).toBeGreaterThan(0)
     expect(userDataHandler.users[0]).toHaveProperty('name')
+  })
+
+  test('getUserEmailsList should throw error when no users', () => {
+    expect(() => userDataHandler.getUserEmailsList())
+      .toThrow('No users loaded!')
+  })
+
+  test('getNumberOfUsers should return 0 number of users', async () => {
+    expect(userDataHandler.getNumberOfUsers()).toBe(0)
+  })
+
+  test('should throw error when no users', async () => {
+    const searchParams = { name: 'Yauhen' }
+    expect(() => userDataHandler.findUsers(searchParams))
+      .toThrow('No users loaded!')
+  })
+})
+
+describe('when users loading failed', () => {
+  beforeAll(async () => {
+    nock.cleanAll()
+    nock(global.SERVER_URL)
+      .get('/users')
+      .replyWithError('Connection refused')
+    userDataHandler = new UserDataHandler()
+  })
+
+  afterAll(() => {
+    nock.cleanAll()
   })
 
   test('loadUsers should throw error when network request fails', async () => {
@@ -52,35 +67,68 @@ describe('General tests', () => {
       .rejects
       .toThrow('Failed to load users data: Error: Connection refused')
   })
+})
 
-  describe('when users are loaded', () => {
-    setupLoadedUsers()
-
-    test('getUserEmailsList should return emails list', async () => {
-      const expectedArrayOfEmails = userDataHandler.users.map(user => user.email)
-      const expectedEmailsList = expectedArrayOfEmails.join(';')
-      const emailsList = userDataHandler.getUserEmailsList()
-      expect(emailsList).toBe(expectedEmailsList)
-    })
-
-    test('getNumberOfUsers should return number of users', async () => {
-      expect(userDataHandler.getNumberOfUsers()).toBe(userDataHandler.users.length)
-    })
+describe('when users were loaded', () => {
+  beforeAll(async () => {
+    setupMock()
+    userDataHandler = new UserDataHandler()
+    await userDataHandler.loadUsers()
   })
 
-  describe('when users are not loaded', () => {
-    test('getUserEmailsList should throw error when no users', () => {
-      expect(() => userDataHandler.getUserEmailsList())
-        .toThrow('No users loaded!')
-    })
+  test('getUserEmailsList should return emails list', async () => {
+    const expectedArrayOfEmails = userDataHandler.users.map(user => user.email)
+    const expectedEmailsList = expectedArrayOfEmails.join(';')
+    const emailsList = userDataHandler.getUserEmailsList()
+    expect(emailsList).toBe(expectedEmailsList)
+  })
 
-    test('getNumberOfUsers should return 0 number of users', async () => {
-      expect(userDataHandler.getNumberOfUsers()).toBe(0)
-    })
+  test('getNumberOfUsers should return number of users', async () => {
+    expect(userDataHandler.getNumberOfUsers()).toBe(userDataHandler.users.length)
+  })
+
+  test('should find user by one parameter', async () => {
+    const existingUserData = userDataHandler.users[0]
+    const searchParams = {
+      username: existingUserData.username
+    }
+    const matchingUsers = userDataHandler.findUsers(searchParams)
+    expect(matchingUsers).toHaveLength(1)
+    expect(matchingUsers[0]).toEqual(existingUserData)
+  })
+
+  test('should find user by multiple parameters', async () => {
+    const existingUserData = userDataHandler.users[1]
+    const searchParams = {
+      name: existingUserData.name,
+      email: existingUserData.email,
+      website: existingUserData.website
+    }
+    const matchingUsers = userDataHandler.findUsers(searchParams)
+    expect(matchingUsers).toHaveLength(1)
+    expect(matchingUsers[0]).toEqual(existingUserData)
+  })
+
+  test('should throw error with no searchParameters argument', async () => {
+    expect(() => userDataHandler.findUsers())
+      .toThrow('No search parameters provoded!')
+  })
+
+  test('should throw error when no matching users', async () => {
+    const searchParams = {
+      name: 'Nonexistent Name',
+      email: 'nothing@void.by'
+    }
+    expect(() => userDataHandler.findUsers(searchParams))
+      .toThrow('No matching users found!')
   })
 })
 
 describe('isMatchingAllSearchParams tests', () => {
+  beforeAll(async () => {
+    userDataHandler = new UserDataHandler()
+  })
+
   test('should match user by name (string)', () => {
     const searchParams = { name: testUserData.name }
     const result = userDataHandler.isMatchingAllSearchParams(testUserData, searchParams)
@@ -136,55 +184,5 @@ describe('isMatchingAllSearchParams tests', () => {
     const result2 = userDataHandler.isMatchingAllSearchParams(testUserData, searchParams2)
     expect(result1).toBe(false)
     expect(result2).toBe(false)
-  })
-})
-
-describe('findUsers tests', () => {
-  describe('when users are loaded', () => {
-    setupLoadedUsers()
-
-    test('should find user by one parameter', async () => {
-      const existingUserData = userDataHandler.users[0]
-      const searchParams = {
-        username: existingUserData.username
-      }
-      const matchingUsers = userDataHandler.findUsers(searchParams)
-      expect(matchingUsers).toHaveLength(1)
-      expect(matchingUsers[0]).toEqual(existingUserData)
-    })
-
-    test('should find user by multiple parameters', async () => {
-      const existingUserData = userDataHandler.users[1]
-      const searchParams = {
-        name: existingUserData.name,
-        email: existingUserData.email,
-        website: existingUserData.website
-      }
-      const matchingUsers = userDataHandler.findUsers(searchParams)
-      expect(matchingUsers).toHaveLength(1)
-      expect(matchingUsers[0]).toEqual(existingUserData)
-    })
-
-    test('should throw error with no searchParameters argument', async () => {
-      expect(() => userDataHandler.findUsers())
-        .toThrow('No search parameters provoded!')
-    })
-
-    test('should throw error when no matching users', async () => {
-      const searchParams = {
-        name: 'Nonexistent Name',
-        email: 'nothing@void.by'
-      }
-      expect(() => userDataHandler.findUsers(searchParams))
-        .toThrow('No matching users found!')
-    })
-  })
-
-  describe('when users are not loaded', () => {
-    test('should throw error when no users', async () => {
-      const searchParams = { name: 'Yauhen' }
-      expect(() => userDataHandler.findUsers(searchParams))
-        .toThrow('No users loaded!')
-    })
   })
 })
