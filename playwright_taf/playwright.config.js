c3da03732571bdc996bd04f6250592eed1bd3b98
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-09-26 22:05:38 +0500

    SC QG updated, one issue fixed

--- playwright_taf/playwright.config.js
+++ playwright_taf/playwright.config.js
@@ -7,14 +7,6 @@ const allureResultsPath = path.join(__dirname, 'artifacts/allure-results');
 const serverUrl = 'http://localhost:3000';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * @see https://playwright.dev/docs/test-configuration
  */
 
